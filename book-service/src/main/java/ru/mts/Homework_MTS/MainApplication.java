16d5c2129e8e2f2f8ccaa5d93c21477a177bb07c
svyatsharik <sharov.so@phystech.edu>
2024-03-04 19:39:20 +0300

    Начало проекта

---
+++ book-service/src/main/java/ru/mts/Homework_MTS/MainApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.Homework_MTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ book-service/src/test/java/ru/mts/Homework_MTS/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.Homework_MTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
